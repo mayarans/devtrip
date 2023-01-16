48d6edcd0815832061e290e2bed3dade1bd37fe1
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-01-15 21:00:58 -0300

    add categories cards section

--- index.js
+++ index.js
@@ -9,3 +9,37 @@ getInputSearch.addEventListener("focus", function () {
 getInputSearch.addEventListener("blur", function () {
   getDivSearch.classList.remove("search-clicked")
 })
+
+/*Movimentação para direita e esquerda da lista de categorias de eventos */
+
+const arrowRight = document.querySelector(".arrow-right")
+const arrowLeft = document.querySelector(".arrow-left")
+const container = document.querySelector(".container-card-and-text")
+container.scrollLeft = 0
+const movimento = 150
+
+function click () {
+  console.log(container.scrollLeft)
+  if (container.scrollLeft >= 200) {
+    container.scrollLeft = 300
+    arrowRight.classList.remove("icon-clicked")
+  } else {
+    arrowRight.classList.add("icon-clicked")
+  }
+  if (container.scrollLeft <=70) {
+    container.scrollLeft = 0
+    arrowLeft.classList.remove("icon-clicked")
+  } else {
+    arrowLeft.classList.add("icon-clicked")
+  }
+}
+
+arrowRight.addEventListener("click", () => {
+  container.scrollLeft+=movimento
+  setTimeout(click, 300)
+})
+
+arrowLeft.addEventListener("click", () => {
+  container.scrollLeft -= movimento
+  setTimeout(click, 300)
+})
