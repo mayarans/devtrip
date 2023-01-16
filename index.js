692e2b6d711b1be31ff25e2e4a745fbf06815a36
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-01-15 22:05:55 -0300

    add login modal and adjusting modal content

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const container = document.querySelector(".container-card-and-text")
 container.scrollLeft = 0
 const movimento = 150
 
-function click () {
+function click() {
   console.log(container.scrollLeft)
   if (container.scrollLeft >= 200) {
     container.scrollLeft = 300
@@ -26,7 +26,7 @@ function click () {
   } else {
     arrowRight.classList.add("icon-clicked")
   }
-  if (container.scrollLeft <=70) {
+  if (container.scrollLeft <= 70) {
     container.scrollLeft = 0
     arrowLeft.classList.remove("icon-clicked")
   } else {
@@ -42,24 +42,24 @@ const getSlides = document.querySelector(".slides")
 
 let count = 0
 
-setInterval( function(){
-  nextImage();
+setInterval(function () {
+  nextImage()
 }, 5000)
 
 getSlides.scrollLeft = 0
-function nextImage () {
+function nextImage() {
   getSlides.scrollLeft += 800
   count++
-  getManualBtn[count-1].classList.remove("manual-btn-clicked")
-  if (count >= getInputRadio.length){
-     count = 0
-     getSlides.scrollLeft = 0
-   }
-   getManualBtn[count].classList.add("manual-btn-clicked") 
+  getManualBtn[count - 1].classList.remove("manual-btn-clicked")
+  if (count >= getInputRadio.length) {
+    count = 0
+    getSlides.scrollLeft = 0
+  }
+  getManualBtn[count].classList.add("manual-btn-clicked")
 }
 
 arrowRight.addEventListener("click", () => {
-  container.scrollLeft+=movimento
+  container.scrollLeft += movimento
   setTimeout(click, 300)
 })
 
@@ -68,22 +68,48 @@ arrowLeft.addEventListener("click", () => {
   setTimeout(click, 300)
 })
 
-/* Abrindo modal de cadastro */
+/* Abrindo modal de cadastro e de login */
+const modalContent = document.querySelector(".modal-content")
+const modalSignUp = document.querySelector(".sign-up-modal")
+const modalLogin = document.querySelector(".login-modal")
+const btnOpenModalSignUp = document.querySelector(".sign-up-button")
+const btnOpenModalLogin = document.querySelector(".login-button")
+const btnCloseModal = document.querySelectorAll(".close-modal-icon")
 
-const modalSignUp = document.querySelector(".modal-content"),
-  btnOpenModalSignUp = document.querySelector(".sign-up-button"),
-  btnCloseModal = document.querySelector(".close-modal-icon")
-
-btnOpenModalSignUp.onclick = function () {
-  modalSignUp.style.display = "flex"
+function openSignUpModal() {
+  modalContent.style.display = "flex"
+  modalSignUp.style.display = "block"
+  modalLogin.style.display = "none"
 }
 
-btnCloseModal.onclick = function () {
+function openLoginModal() {
+  modalContent.style.display = "flex"
+  modalLogin.style.display = "block"
   modalSignUp.style.display = "none"
 }
 
+function closeModals() {
+  modalContent.style.display = "none"
+  modalLogin.style.display = "none"
+  modalSignUp.style.display = "none"
+}
+
+btnOpenModalSignUp.addEventListener("click", openSignUpModal)
+btnOpenModalLogin.addEventListener("click", openLoginModal)
+
+btnCloseModal.forEach((button, i) => {
+  btnCloseModal[i].addEventListener("click", closeModals)
+})
+
 window.onclick = function (event) {
-  if (event.target == modalSignUp) {
-    modalSignUp.style.display = "none"
+  if (event.target == modalContent) {
+    closeModals()
   }
 }
+
+/* Trocando de modals */
+const buttonChangeSignUpToLoginModal = document.querySelector("#login")
+const buttonChangeLoginToSignUpModal = document.querySelector("#signup")
+
+buttonChangeSignUpToLoginModal.addEventListener("click", openLoginModal)
+buttonChangeLoginToSignUpModal.addEventListener("click", openSignUpModal)
